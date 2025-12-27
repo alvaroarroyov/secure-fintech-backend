80ddd749b195be6796d724f5ad97a64f9c1d85a7
Alvaro Arroyo <vasquezalvarox2003@gmail.com>
2025-12-27 02:03:56 +0000

    feat: Initial DevSecOps setup with K8s and CI/CD

---
+++ src/main/java/com/security/app/SecureFintechBackendApplication.java
@@ -0,0 +1,14 @@
+package com.security.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFintechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFintechBackendApplication.class, args);
+		System.out.println("Apllication Successfully Launched");
+	}
+
+}
